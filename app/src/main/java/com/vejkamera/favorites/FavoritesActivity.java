b7bd308f1b368048505889e1de51c3e4f7942ddc
andersdinesen <andersdinesen@users.noreply.github.com>
2015-10-28 15:32:24 +0100

    More work on the Drawer Menu

--- app/src/main/java/com/vejkamera/favorites/FavoritesActivity.java
+++ app/src/main/java/com/vejkamera/favorites/FavoritesActivity.java
@@ -1,26 +1,36 @@
 package com.vejkamera.favorites;
 
+import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.res.Configuration;
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.vejkamera.area.AreasListActivity;
 import com.vejkamera.R;
 import com.vejkamera.RoadCamera;
 import com.vejkamera.details.RoadCameraDetailsActivity;
+import com.vejkamera.favorites.adapter.NavDrawerItem;
+import com.vejkamera.favorites.adapter.NavDrawerListAdapter;
 import com.vejkamera.map.RoadCamersMapsActivity;
 import com.vejkamera.services.RoadCameraImageReaderService;
 import com.vejkamera.services.RoadCameraLoopReaderService;
@@ -34,6 +44,8 @@ public class FavoritesActivity extends AppCompatActivity {
     FavoritesResponseReceiver favoritesResponseReceiver = new FavoritesResponseReceiver();
     Intent readIntent = null;
     private ActionBarDrawerToggle mDrawerToggle;
+    DrawerLayout mDrawerLayout;
+    ListView mDrawerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,55 +56,73 @@ public class FavoritesActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
 
-        //setupDrawerMenu();
+        setupDrawerMenu(savedInstanceState);
         setupAdapter();
     }
 
-//    private void setupDrawerMenu() {
-//        String[] mPlanetTitles = getResources().getStringArray(R.array.planets);
-//        ListView mDrawerList = (ListView) findViewById(R.id.left_drawer);
-//
-//        // Set the adapter for the list view
-//        mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, mPlanetTitles));
-//        // Set the list's click listener
-//        //mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
-//
-//
-//        DrawerLayout mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-//        mDrawerToggle = new ActionBarDrawerToggle(
-//                this,                  /* host Activity */
-//                mDrawerLayout,         /* DrawerLayout object */
-//                //R.drawable.ic_drawer,  /* nav drawer icon to replace 'Up' caret */
-//                R.string.drawer_open,  /* "open drawer" description */
-//                R.string.drawer_close  /* "close drawer" description */
-//        ) {            /** Called when a drawer has settled in a completely closed state. */
-//        public void onDrawerClosed(View view) {
-//            super.onDrawerClosed(view);
-//            //getActionBar().setTitle(mTitle);
-//        }
-//
-//            /** Called when a drawer has settled in a completely open state. */
-//            public void onDrawerOpened(View drawerView) {
-//                super.onDrawerOpened(drawerView);
-//                //getActionBar().setTitle(mDrawerTitle);
-//            }
-//        };
-//        // Set the drawer toggle as the DrawerListener
-//        mDrawerLayout.setDrawerListener(mDrawerToggle);
-//
-//        getActionBar().setDisplayHomeAsUpEnabled(true);
-//        getActionBar().setHomeButtonEnabled(true);
-//    }
+    private void setupDrawerMenu(Bundle savedInstanceState) {
+        final CharSequence mTitle = getTitle();
+        final CharSequence mDrawerTitle = mTitle;
+
+        String[] navMenuTitles = getResources().getStringArray(R.array.nav_drawer_items);
+
+        // nav drawer icons from resources
+        TypedArray navMenuIcons = getResources().obtainTypedArray(R.array.nav_drawer_icons);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.list_slidermenu);
+
+        ArrayList<NavDrawerItem> navDrawerItems = new ArrayList<NavDrawerItem>();
+
+        // adding nav drawer items to array
+        navDrawerItems.add(new NavDrawerItem(navMenuTitles[0], navMenuIcons.getResourceId(0, -1)));
+        navDrawerItems.add(new NavDrawerItem(navMenuTitles[1], navMenuIcons.getResourceId(1, -1)));
+        navDrawerItems.add(new NavDrawerItem(navMenuTitles[2], navMenuIcons.getResourceId(2, -1)));
+        navDrawerItems.add(new NavDrawerItem(navMenuTitles[3], navMenuIcons.getResourceId(3, -1)));
+        navDrawerItems.add(new NavDrawerItem(navMenuTitles[4], navMenuIcons.getResourceId(4, -1)));
+        navDrawerItems.add(new NavDrawerItem(navMenuTitles[5], navMenuIcons.getResourceId(5, -1)));
+
+        // Recycle the typed array
+        navMenuIcons.recycle();
+
+        NavDrawerListAdapter drawerListAdapter = new NavDrawerListAdapter(getApplicationContext(), navDrawerItems);
+        mDrawerList.setAdapter(drawerListAdapter);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.string.app_name, // nav drawer open - description for accessibility
+                R.string.app_name // nav drawer close - description for accessibility
+        ){
+            public void onDrawerClosed(View view) {
+                getSupportActionBar().setTitle(mTitle);
+                // calling onPrepareOptionsMenu() to show action bar icons
+                invalidateOptionsMenu();
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                getSupportActionBar().setTitle(mDrawerTitle);
+                // calling onPrepareOptionsMenu() to hide action bar icons
+                invalidateOptionsMenu();
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        mDrawerList.setOnItemClickListener(new SlideMenuClickListener());
+
+    }
+
 
     @Override
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
         updateFavorites();
         readFavoriteCameras();
     }
 
     @Override
-    protected void onPause(){
+    protected void onPause() {
         super.onPause();
         LocalBroadcastManager.getInstance(getBaseContext()).unregisterReceiver(favoritesResponseReceiver);
         // For some reason a waiting service does not stop on stopService. Broadcasting stop intent instead.
@@ -108,7 +138,7 @@ public class FavoritesActivity extends AppCompatActivity {
         favorites.add(new RoadCamera("E20 Kauslunde V", "http://webcam.trafikken.dk/webcam/kauslunde2.jpg", null));*/
     }
 
-    private void setupAdapter(){
+    private void setupAdapter() {
         final ListView listView = (ListView) findViewById(R.id.favorites_listview);
         adapter = new FavoriteListAdapter(this, favorites);
         listView.setAdapter(adapter);
@@ -153,18 +183,56 @@ public class FavoritesActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
-        } else if (id == R.id.action_by_list){
+        } else if (id == R.id.action_by_list) {
             Intent intent = new Intent(this, AreasListActivity.class);
             startActivity(intent);
-        } else  if (id == R.id.action_by_map){
+        } else if (id == R.id.action_by_map) {
             Intent intent = new Intent(this, RoadCamersMapsActivity.class);
             startActivity(intent);
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /***
+     * Called when invalidateOptionsMenu() is triggered
+     */
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // if nav drawer is opened, hide the action items
+        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+        menu.findItem(R.id.action_settings).setVisible(!drawerOpen);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        getActionBar().setTitle(title);
+    }
+
+    /**
+     * When using the ActionBarDrawerToggle, you must call it during
+     * onPostCreate() and onConfigurationChanged()...
+     */
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggls
+        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 
     private class FavoritesResponseReceiver extends BroadcastReceiver {
@@ -180,4 +248,24 @@ public class FavoritesActivity extends AppCompatActivity {
 
         }
     }
+
+    /**
+     * Slide menu item click listener
+     * */
+    private class SlideMenuClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // display view for selected nav drawer item
+            switch (position) {
+                case 1:
+                    Intent intent = new Intent(parent.getContext(), AreasListActivity.class);
+                    startActivity(intent);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
 }
+
+
